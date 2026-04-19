70aaeac4d6b127794b634aa39ab35979b57768d3
Trindade <gamaflavio1000@gmail.com>
2026-04-19 03:14:05 -0300

    Add trigger to update price and log changes

--- 02_triggers.sql
+++ 02_triggers.sql
@@ -11,3 +11,15 @@ end$$
 
 DELIMITER ;
 
+DELIMITER $$
+create trigger atualizar_preco
+after UPDATE on Produtos
+for each row
+begin
+IF old.preco != new.preco
+THEN
+INSERT INTO Auditoria (usuario_id,tabela_afetada,id_registro,tipo_operacao,valor_antigo,valor_novo) VALUES (@usuario_id,'Produtos',NEW.id,'UPDATE',old.preco,new.preco);
+END IF;
+end$$
+
+DELIMITER ;
