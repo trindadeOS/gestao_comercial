6673fe4b2004eec2636f85b4f78cd4c72803c7f4
Trindade <gamaflavio1000@gmail.com>
2026-04-20 02:31:44 -0300

    sem alterações

--- 02_triggers.sql
+++ 02_triggers.sql
@@ -16,7 +16,7 @@ create trigger atualizar_preco
 after UPDATE on Produtos
 for each row
 begin
-IF old.preco != new.preco
+IF old.preco <> new.preco
 THEN
 INSERT INTO Auditoria (usuario_id,tabela_afetada,id_registro,tipo_operacao,valor_antigo,valor_novo) VALUES (@usuario_id,'Produtos',NEW.id,'UPDATE',old.preco,new.preco);
 END IF;
